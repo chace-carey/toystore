c666c5f66281c3c5f4342bce0a423d790cc04935
Chace Carey <chace10@chaces-mbp.mynetworksettings.com>
2024-04-07 21:53:34 -0400

    Working database application

--- order.php
+++ order.php
@@ -10,6 +10,18 @@
 	 		  - Execute the SQL query using the pdo function and fetch the result
 	 		  - Return the order info
 	 */
+	function get_order(PDO $pdo, string $email, string $orderNum){
+		$sql = "SELECT orders.*, customer.*
+		FROM orders
+		JOIN customer ON orders.custnum = customer.custnum
+		WHERE email = :email AND orderNum = :orderNum";
+
+		$stmt = $pdo->prepare($sql);
+		$stmt->execute(['email' => $email, 'orderNum' => $orderNum]);
+		$order = $stmt->fetch(PDO::FETCH_ASSOC);
+
+		return $order;
+	 }
 
 	
 	// Check if the request method is POST (i.e, form submitted)
@@ -25,6 +37,8 @@
 		/*
 		 * TO-DO: Retrieve info about order from the db using provided PDO connection
 		 */
+
+		$order = get_order($pdo, $email, $orderNum);
 		
 	}
 // Closing PHP tag  ?> 
@@ -89,19 +103,19 @@
 				  -- TO-DO: Check if variable holding order is not empty. Make sure to replace null with your variable!
 				  -->
 				
-				<?php if (!empty(null)): ?>
+				<?php if (!empty($order)): ?>
 					<div class="order-details">
 
 						<!-- 
 				  		  -- TO DO: Fill in ALL the placeholders for this order from the db
   						  -->
 						<h1>Order Details</h1>
-						<p><strong>Name: </strong> <?= '' ?></p>
-				        	<p><strong>Username: </strong> <?= '' ?></p>
-				        	<p><strong>Order Number: </strong> <?= '' ?></p>
-				        	<p><strong>Quantity: </strong> <?= '' ?></p>
-				        	<p><strong>Date Ordered: </strong> <?= '' ?></p>
-				        	<p><strong>Delivery Date: </strong> <?= '' ?></p>
+						<p><strong>Name: </strong> <?= $order['cname'] ?></p>
+				        	<p><strong>Username: </strong> <?= $order['username'] ?></p>
+				        	<p><strong>Order Number: </strong> <?= $order['ordernum'] ?></p>
+				        	<p><strong>Quantity: </strong> <?= $order['quantity'] ?></p>
+				        	<p><strong>Date Ordered: </strong> <?= $order['date_ordered'] ?></p>
+				        	<p><strong>Delivery Date: </strong> <?= $order['date_deliv'] ?></p>
 				      
 					</div>
 				<?php endif; ?>
